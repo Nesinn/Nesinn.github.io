43a8becf44da9f9bc30968cdfa24aa8b477282eb
Nesinn <halliskjol@gmail.com>
2018-04-12 09:21:13 +0000

    displaying my dinner plans

--- resource/javascript/main.js
+++ resource/javascript/main.js
@@ -11,6 +11,10 @@ var Calendar = {
   iceMonths : ["Janúar", "Febrúar", "Mars", "Apríl", "Maí", "Juní", "Julí", "Ágúst", "September", "Október", "Nóvember", "Desember"],
   //Names of the Days in Icelandic
   iceDays : ["Sunnudagur", "Mánudagur", "Þriðjudagur", "Miðvikudagur", "Fimtudagur", "Föstudagur", "Laugardagur"],
+  //Returns true if the input date is a date
+  isDate : function(date){
+    return (date instanceof Date);
+  },
   //Ruturns Day of Week Day og month and month
   getDayMdayMonth : function(newDate){
     return this.iceDays[newDate.getDay()] + " " + newDate.getDate() + ". " + this.iceMonths[newDate.getMonth()];
@@ -19,46 +23,66 @@ var Calendar = {
   getClock : function(){
     var today = new Date();
     var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    if(m < 10) {
-      m = "0" + m;
-    }
-    if(s < 10) {
-      s = "0" + s;
-    }
+    var m = addZero(today.getMinutes());
+    var s = addZero(today.getSeconds());
     return (h + ":" + m + ":" + s);
+  },
+  //Returns the number with 0 if it´s below 10.
+  addZero : function(num){
+    if(num < 10){
+      return "0" + num;
+    } else {
+      return "" + num;
+    }
+  },
+  getDayID : function(date){
+    if(this.isDate(date)){
+      var d = "" + date.getDate();
+      var m = this.addZero(date.getMonth() + 1);
+      var y = this.addZero(date.getYear() + 1900);
+      return (d + m + y);
+    } else {
+      return date;
+    }
   },
   // returns the day before given date.
   yesterday : function(date) {
-    if(date.getDate() === 1) {
-      //First day of the month, so yesterday would be previous month.
-      if(date.getMonth() === 0) {
-        //if its January we need to change the year.
-        return (new Date(date.getYear() + 1899, 12, 0));
+    if(this.isDate(date)){
+      if(date.getDate() === 1) {
+        //First day of the month, so yesterday would be previous month.
+        if(date.getMonth() === 0) {
+          //if its January we need to change the year.
+          return (new Date(date.getYear() + 1899, 12, 0));
+        } else {
+          //it's not January, so only change month.
+          return (new Date(date.getYear() + 1900, date.getMonth(), 0));
+        }
       } else {
-        //it's not January, so only change month.
-        return (new Date(date.getYear() + 1900, date.getMonth(), 0));
+        //it's not the first day of the month.
+        return (new Date(date.getYear() + 1900, date.getMonth(), date.getDate() - 1));
       }
     } else {
-      //it's not the first day of the month.
-      return (new Date(date.getYear() + 1900, date.getMonth(), date.getDate() - 1));
+      return date;
     }
   },
   // returns the day after given date.
   tomorrow : function(date) {
-    //if its the last day of the month, change month
-    if(date.getDate() === 0) {
-      //if the month is Desember, also change the year
-      if(date.getMonth() === 12) {
-        return (new Date(date.getYear() + 1901, 1, 1));
+    if(this.isDate(date)){
+      //if its the last day of the month, change month
+      if(date.getDate() === 0) {
+        //if the month is Desember, also change the year
+        if(date.getMonth() === 12) {
+          return (new Date(date.getYear() + 1901, 1, 1));
+        } else {
+          //last day of the month, but not Desember
+          return (new Date(date.getYear() + 1900, date.getMonth() + 1, 1));
+        }
       } else {
-        //last day of the month, but not Desember
-        return (new Date(date.getYear() + 1900, date.getMonth() + 1, 1));
+        //not the last day of the month.
+        return (new Date(date.getYear() + 1900, date.getMonth(), date.getDate() + 1));
       }
     } else {
-      //not the last day of the month.
-      return (new Date(date.getYear() + 1900, date.getMonth(), date.getDate() + 1));
+      return date;
     }
   }
 };
@@ -142,7 +166,7 @@ var DATABASE = {
             ],
     USER: [{
       startDate: "Wed Apr 11 2018 13:07:57 GMT+0000 (Greenwich Standard Time)",
-      dateID : [],
+      dateID : ["13042018", "14042018", "15042018", "16042018", "17042018"],
       dinnerPlans : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33]
     }]
   }
@@ -185,7 +209,7 @@ var Menu = {
 //Heimahjalp page object
 var HomeHelp = {
   userID : -1,
-  displayDay : "",
+  //displayDay : "",
   displayDinner : -1,
   display : function() {
     //We need the username before setting up the page
@@ -209,16 +233,11 @@ var HomeHelp = {
         //if user clicks arrow, we need to change the date.
         $("div.arrow:first").on('click', function(e){
           //since he clicked the left arrow go back a day.
-          if(displayDinner === 0){
-            displayDinner = DATABASE.HOMEHELP.USER[userID].dinnerPlans.length;
-          } else {
-            displayDinner = displayDinner - 1;
-          }
-          HomeHelp.setDay(Calendar.yesterday(displayDay), displayDinner);
+          HomeHelp.setDay(Calendar.yesterday(displayDay));
         });
         $("div.arrow:last").on('click', function(e){
           //since he clicked the right arrow go ahead a day.
-          HomeHelp.setDay(Calendar.tomorrow(displayDay), displayDinner + 1);
+          HomeHelp.setDay(Calendar.tomorrow(displayDay));
         });
         displayDay = new Date;
         this.setDay(displayDay);
@@ -226,16 +245,37 @@ var HomeHelp = {
         //user does not exist
       }
     }
-  },  
+  },
   //gets a date and changes accoringly.
-  setDay : function(date, dinnerID) {
+  setDay : function(date) {
     //check newDate if it is a date!
-    displayDay = date;
-    dishNumber = DATABASE.HOMEHELP.USER[userID].startDate - new Date;
-    console.log(dishNumber);
-    var dinner = DATABASE.HOMEHELP.DINNER[DATABASE.HOMEHELP.USER[userID].dinnerPlans[0]].name;
-    $(".dinner").text(dinner);
-    $("#date").text(Calendar.getDayMdayMonth(displayDay));
+    if(Calendar.isDate(date)){
+      //save the date for the arrows.
+      displayDay = date;
+      //get the day ID (looks like a social securaty number).
+      var dayID = Calendar.getDayID(date);
+      //we need to set these incase we dont have food plans.
+      var dinnerIDforToday = -1;
+      var dinner = "";
+      //check if this user has food plans for the day
+      for(var i = 0 ; i < DATABASE.HOMEHELP.USER[userID].dateID.length ; i++){
+        if(DATABASE.HOMEHELP.USER[userID].dateID[i] === dayID){
+          //the user has a plan. then there is no need to continue searching.
+          dinnerIDforToday = i;
+          i = DATABASE.HOMEHELP.USER[userID].dateID.length;
+        }
+      }
+      if(dinnerIDforToday >= 0){
+        //since he has a plan we can get the name of the dinner
+        dinner = DATABASE.HOMEHELP.DINNER[DATABASE.HOMEHELP.USER[userID].dinnerPlans[dinnerIDforToday]].name;
+      }
+      //we change the text on the page, if there was no plan the string will be empty
+      $(".dinner").text(dinner);
+      $("#date").text(Calendar.getDayMdayMonth(date));
+    } else {
+      //the date test failed!
+      $("#date").text("Error: date value for setDay was not a date (" + date + ")");
+    }
   }
 };
 
